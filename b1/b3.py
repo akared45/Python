4601f5af217289f78702740437aa8418d01484c1
LE THANH VINH <131985083+akared45@users.noreply.github.com>
2025-07-18 10:37:44 +0700

    Update b3.py

--- b1/b3.py
+++ b1/b3.py
@@ -1,7 +1,8 @@
-array = [1, 2, 3, 4]
-doubled_array = []
-
-for i in array:
-    doubled_array.append(i * 2)
-
-print(doubled_array)
+def double():    
+    array = [1, 2, 3, 4]
+    doubled_array = []
+    for i in array:
+        doubled_array.append(i * 2)
+    print(doubled_array)
+if __name__ == '__main__':
+    double()

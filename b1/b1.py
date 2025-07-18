090042681b46c566c25e52897f79212b87e991d1
LE THANH VINH <131985083+akared45@users.noreply.github.com>
2025-07-18 10:35:01 +0700

    Update b1.py

--- b1/b1.py
+++ b1/b1.py
@@ -1,5 +1,8 @@
-num = int(input("Nhap"))
-if num % 2 == 0 :
-    print("Chan")
-else:
-    print("Le")
+def OddEven():    
+    num = int(input("Nhap"))
+    if num % 2 == 0 :
+        print("Chan")
+    else:
+        print("Le")
+if __name__ == '__main__':
+    OddEven()

92ed13f7b801b31715b40f27c4cc8814fbfa8c81
LE THANH VINH <131985083+akared45@users.noreply.github.com>
2025-07-22 07:16:02 +0700

    Update b1.py

--- b1/b1.py
+++ b1/b1.py
@@ -1,5 +1,9 @@
-num = int(input("Nhap"))
-if num % 2 == 0 :
-    print("Chan")
-else:
-    print("Le")
+def check_even_odd():
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+if __name__ == "__main__":
+    check_even_odd()
